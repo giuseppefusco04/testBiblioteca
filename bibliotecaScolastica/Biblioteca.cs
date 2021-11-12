1b1867a9db3e13b381f8dabe9e3332d80a0b7407
Giuseppe Fusco <GIUSEPPE.FUSCO@local.ittcesena.it>
2021-11-12 08:46:26 +0100

    v1.5:aggiunta metodo aggiungilibro

--- bibliotecaScolastica/Biblioteca.cs
+++ bibliotecaScolastica/Biblioteca.cs
@@ -6,5 +6,28 @@ namespace bibliotecaScolastica
 {
     class Biblioteca
     {
+        /*Branch Biblioteca: sviluppare la classe biblioteca che consenta di memorizzare il nome, l’indirizzo e gli orari di
+apertura e chiusura giornaliera, che contenga l’elenco dei libri e consenta l’utilizzo delle operazioni sopra
+elencate.
+         */
+        private string _nome, _indirizzo, _orarioApertura,_orarioChiusura;
+        private List<Libro> _libri;
+        public string Nome { get; set; }
+        public string Indirizzo { get; set; }
+        public string OrarioApertura { get; set; }
+        public string OrarioChiusura { get; set; }
+        public Biblioteca(string n,string i,string oa,string oc)
+        {
+            Nome = n;
+            Indirizzo = i;
+            OrarioApertura = oa;
+            OrarioChiusura = oc;
+            _libri = new List<Libro>();
+        }
+        public void AggiungiLibro(Libro l)
+        {
+            _libri.Add(l);
+        }
+
     }
 }
